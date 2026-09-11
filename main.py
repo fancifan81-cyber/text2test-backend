4c7a5a5ff4f79ce4c942be7496b01fcc9372f1f0
fancifan81-cyber <fancifan81@gmail.com>
2026-09-12 00:41:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 import fitz
 from google import genai
+from supabase import create_client, Client
 
 
 app = FastAPI()
+# =========================
+# SUPABASE
+# =========================
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 
 
 # =========================
