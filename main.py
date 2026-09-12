f9cb8d8fd902b9c24df5093b78dee5d408a3f49d
fancifan81-cyber <fancifan81@gmail.com>
2026-09-12 19:30:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,15 @@ app = FastAPI()
 # SUPABASE
 # =========================
 
+from supabase import create_client, Client
+
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 
 if not SUPABASE_URL:
     raise RuntimeError("SUPABASE_URL is not set in environment variables")
